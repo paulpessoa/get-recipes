2fe181993e2ce91e6588691753fe7918a981669f
Paul Pessoa <74559558+paulpessoa@users.noreply.github.com>
2024-10-06 21:44:50 -0300

    refresh url

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://aqmyzkjbxdnotbtxxlkv.supabase.co/rest/v1';
-axios.defaults.headers.common['apikey'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFxbXl6a2pieGRub3RidHh4bGt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjU0NjM4NjIsImV4cCI6MTk4MTAzOTg2Mn0.B4kxw-I33WsCrC5fi7gL4sQ1Z1jpeCjMnD1yyGHkrbM';
+axios.defaults.baseURL = 'https://tsxmljhumrcevktlgqvx.supabase.co/rest/v1';
+axios.defaults.headers.common['apikey'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzeG1samh1bXJjZXZrdGxncXZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc2MjUzODMsImV4cCI6MjA0MzIwMTM4M30.LOTc1YS5ws1MdsJ9RCpumU1xt4_ZQWNsyxqgZjyh8fg';
 
 createApp(App).use(store).use(router).mount('#app')
